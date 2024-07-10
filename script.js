7c6e7a768618d5ad38c968dc30d14ce1c6a0b044
Ankit Saraswat <152775694+saraswatAnkit@users.noreply.github.com>
2024-07-10 11:48:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,54 +1,57 @@
 const accessKey = "nQh859x6qGfgGQ3_EtbutCdAbHHoGCsvHEOReo4F2GU";
 
-const formE1 = document.querySelector("form");
-const inputE1 = document.getElementById("search-input");
-const searchResults = document.querySelector(".search-results");
-const showMore = document.getElementById("show-more-button");
+const formE1 = document.querySelector("form")
+const inputE1 = document.getElementById("search-input")
+const searchResults = document.querySelector(".search-results")
+const showMore = document.getElementById("show-more-btn")
 
-let inputData = "";
-let page = 1;
+let inputData = ""
+let page = 1
 
 async function searchImages(){
-    inputData = inputE1.value;
+    inputData = inputE1.value
     const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
 
-    const response = await fetch(url);
-    const data = await response.json();
+    const response = await fetch(url)
+    const data = await response.json()
 
-    const results = data.results;
+    const results =data.results;
 
     if(page == 1){
         searchResults.innerHTML = ""
     }
 
-    results.map((result) => {
+    results.map((result)=> {
         const imageWrapper = document.createElement('div');
         imageWrapper.classList.add("search-box");
-        const image = document.createElement('img');
-        image.src = result.urls.small;
-        image.alt = result.alt_description;
-        const imageLink = document.createElement('a');
+        const boxLink = document.createElement('a')
+        boxLink.href = result.links.html;
+        boxLink.target = "_blank"
+        const image = document.createElement('img')
+        image.src = result.urls.small
+        image.alt = result.alt_description
+        const imageLink = document.createElement('a')
         imageLink.href = result.links.html;
         imageLink.target = "_blank"
         imageLink.textContent = result.alt_description;
 
-        imageWrapper.appendChild(image);
-        imageWrapper.appendChild(imageLink);
-        searchResults.appendChild(imageWrapper);
+        boxLink.appendChild(image)
+        imageWrapper.appendChild(boxLink)
+        imageWrapper.appendChild(imageLink)
+        searchResults.appendChild(imageWrapper)
     });
 
     page++
     if(page>1){
-        showMore.style.display = "block"
+        showMore.style.display= "block"
     }
 }
-
-formE1.addEventListener("submit", (event) =>{
-    event.preventDefault();
-    page = 1;
+formE1.addEventListener("submit",(e) => {
+    e.preventDefault();
+    page = 1
     searchImages();
 })
 
-showMore.addEventListener("click", () =>{
+showMore.addEventListener("click",()=>{
     searchImages();
 })
